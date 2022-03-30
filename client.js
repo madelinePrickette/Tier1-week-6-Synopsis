9000c913c0325e5862025f1140c22e4077be7fbf
Madeline Prickette <madelineprickette@gmail.com>
2022-03-30 12:40:20 -0500

    addedd 25000 to the budget

--- client.js
+++ client.js
@@ -2,6 +2,13 @@ console.log( 'js' );
 
 $( document ).ready( readyNow );
 
+const budget = 25000;
+
 function readyNow(){
-    console.log( 'JQ' );
+// console.log( 'JQ' );
+    //display budget
+    //target budgetOut by id
+    let el = $( '#budgetOut' );
+    el.empty();
+    el.append( budget );
 }//end readyNow
